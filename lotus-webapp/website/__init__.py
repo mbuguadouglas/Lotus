f6d08232ef16ed57dcdef59e469ff2a78b579641
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-03-31 13:06:25 +0300

    create the website using sqlite3

--- lotus-webapp/website/__init__.py
+++ lotus-webapp/website/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+# from os import path
+
+db = SQLAlchemy()
+db_name = 'db.sqlite3'
+
 
 def create_app():
+	""" function to runn the application"""
+
 	# initialize the flask app
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = '@Th3NorthR3m3mbers'	#set secret key for encrypting sessions
+	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
 
+	db.init_app(app)	#initialize database
 
 	# import the various blueprints and register them
 	from .auth import auth
@@ -19,6 +29,27 @@ def create_app():
 	# app.register_blueprint(admin)
 	app.register_blueprint(admin, url_prefix='/admin')	#localhost:5000/admin/<route>
 
-	# from .models import
+	from .models import Customer, Product, Cart, Order
+	with app.app_context():
+		create_database(app)
+		
 
 	return app
+
+
+def create_database(app):
+	db.create_all()
+	return 'Database created successfully!'
+
+
+# # FUNCTION TO CREATE DB. MUST IMPORT OS PATH
+# # FIND WHAT IS THE DIFFERENCE BTWN THE TWO AND WHICH IS BETTER FOR OPTIMIZATION
+# def create_database(app):
+# 	"""function to create the database"""
+
+# 	with app.app_context():
+# 		if not path.exists('website'+db_name):
+# 			db.create_all()
+# 			print ('created database succesfully!')
+# 		else:
+# 			print ('An error occurred during database creation')
---
+++ lotus-webapp/website/models.py
@@ -0,0 +1,99 @@
+from . import db 
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# customer table for all the customers
+class Customer(db.Model, UserMixin):
+	__tablename__ = 'customers'
+
+	id = db.Column(db.Integer, primary_key=True, index=True)
+	email = db.Column(db.String(50), unique=True , nullable=False)
+	name = db.Column(db.String(50))
+	password_hash = db.Column(db.String(50))
+	date_joined = db.Column(db.DateTime(), default=datetime.utcnow)
+
+	# setup foreign keys
+	orders = db.Relationship('Order', backref=db.backref('customers'))	# 1.link customer to order
+	cart_item = db.Relationship('Cart', backref=db.backref('customers'))		# 2.link customer to cart
+	# product = db.Relationship('Product', backref=db.backref('customer'))		# 3.link customer to product table
+
+
+	# use getter and setter methods to set password hash and allow for its retrieval
+	@property
+	def password(self):
+		raise AttributError('Password is not readable')
+	
+	# setter
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password=password)
+
+	# password verification
+	def verify_password(self,password):
+		# if passwords match(correct) return true, else return false
+		return check_password_hash(self.password_hash, password=password)
+
+	# string method to allow for better represenattion of class
+	def __str__(self):
+		#string formatiing. see if you can use f Strings
+		#f'<Customer {Customer.id}' 
+		return '<Customer %r >' % Customer.id
+
+
+# product table for all the shop items
+class Product(db.Model):
+	__tablename__ = 'products'
+
+	id = db.Column(db.Integer, primary_key=True, index=True)
+	name = db.Column(db.String(50), nullable=False)
+	picture = db.Column(db.String(1000))	#why is the picture set as a string?!
+	category = db.Column(db.String(50))
+	current_price = db.Column(db.Float(), nullable=False)
+	past_price = db.Column(db.Float(), nullable=False)
+	instock = db.Column(db.Integer())
+	flash_sale = db.Column(db.Boolean, default=False)
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+	# setup and foreign key with cart & order table
+	carts = db.Relationship('Cart', backref=db.backref('products'))
+	orders = db.Relationship('Order', backref=db.backref('products'))
+
+	# __str__ method to return human redable form of product
+	def __str__(self):
+		# are fStrings possible?
+		return '<Product %r>' % self.name
+
+class Cart(db.Model):
+	__tablename__ = 'carts'
+
+	id = db.Column(db.Integer, primary_key=True)
+	items = db.Column(db.Integer, nullable=False)
+
+	# setup foreigh keys
+	customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+	products = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+	def __str__(self):
+		# return f'<Cart{self.id}'
+		return '<Cart %r>' % self.id
+
+class Order(db.Model):
+	__tablename__ = 'orders'
+
+	id = db.Column(db.Integer, primary_key=True)
+	items = db.Column(db.Integer, nullable=False)
+	price = db.Column(db.Float, nullable=False)
+	status = db.Column(db.String, nullable=False)
+	payment_id = db.Column(db.String(1000))
+
+	# setup foreign key
+	products = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+	def __str__(self):
+		# return f'Order {self.name}'
+		return '<Order %r>' % self.name
+
+
+
